fdd5da71b4179dea36e1143e99758ca99127158f
Friederike Wild <dev@friederikewild.me>
2013-06-26 11:24:31 +0100

    * Added a short information dialog available via the simple menu including a link to the github project

--- sample/src/de/devmob/apprater/demo/MainActivity.java
+++ sample/src/de/devmob/apprater/demo/MainActivity.java
@@ -6,9 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
@@ -59,8 +64,64 @@ public class MainActivity extends Activity
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        getMenuInflater().inflate(R.menu.layout_main, menu);
+        super.onCreateOptionsMenu(menu);
+        this.getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    /* (non-Javadoc)
+     * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+     */
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.menu_settings:
+            {
+
+                return true;
+            }
+            case R.id.menu_info:
+            {
+                showDialog(R.id.dialog_info);
+                return true;
+            }
+        }
+
+        // Default handling
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* (non-Javadoc)
+     * @see android.app.Activity#onCreateDialog(int)
+     */
+    protected Dialog onCreateDialog(int id)
+    {
+        switch (id)
+        {
+            case R.id.dialog_info:
+            {
+                // Create the requested dialog
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this)
+                        .setIcon(R.drawable.ic_launcher)
+                        .setTitle(R.string.menu_info)
+                        .setNeutralButton(android.R.string.ok, null);
+
+                // Add a custom text view to enable a clickable link
+                LayoutInflater layoutInflater = LayoutInflater.from(this);        
+                final View textEntryView = layoutInflater.inflate(R.layout.layout_text_dialog, null);
+                TextView messageTextView = ((TextView) textEntryView.findViewById(R.id.textMessage));
+                messageTextView.setText(Html.fromHtml(this.getResources().getString(R.string.text_info)));
+                messageTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+                dialogBuilder.setView(textEntryView);
+
+                return dialogBuilder.create();
+            }
+        }
+
+        return null;
     }
 
     /**
